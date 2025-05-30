aaf25d1f5627e7aacd92fbb61154df45c66bac88
Anna <annachristinethomas@gmail.com>
2025-05-30 11:29:11 -0700

    Filled out customers page

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,9 +1,24 @@
+import CustomersTable from '@/app/ui/customers/table';
 import { Metadata } from 'next';
+import { CustomersTableSkeleton } from '@/app/ui/skeletons';
+import { fetchCustomersAllData } from '@/app/lib/data';
+import { Suspense } from 'react';
  
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-    return <p>Customers Page</p>;
+export default async function Page() {
+
+    const customers = await fetchCustomersAllData();
+
+    return (
+        <div className="w-full">
+            <div>
+                <Suspense fallback={<CustomersTableSkeleton />}>
+                    <CustomersTable customers={customers} />
+                </Suspense>
+            </div>
+        </div>
+    );
 }
--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -184,6 +184,36 @@ export async function fetchCustomers() {
   }
 }
 
+export async function fetchCustomersAllData() {
+  try {
+    const data = await sql<CustomersTableType[]>`
+		SELECT
+		  customers.id,
+		  customers.name,
+		  customers.email,
+		  customers.image_url,
+		  COUNT(invoices.id) AS total_invoices,
+		  SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
+		  SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END) AS total_paid
+		FROM customers
+		LEFT JOIN invoices ON customers.id = invoices.customer_id
+		GROUP BY customers.id, customers.name, customers.email, customers.image_url
+		ORDER BY customers.name ASC
+	  `;
+
+    const customers = data.map((customer) => ({
+      ...customer,
+      total_pending: formatCurrency(customer.total_pending),
+      total_paid: formatCurrency(customer.total_paid),
+    }));
+
+    return customers;
+  } catch (err) {
+    console.error('Database Error:', err);
+    throw new Error('Failer to fetch all customer data.');
+  }
+}
+
 export async function fetchFilteredCustomers(query: string) {
   try {
     const data = await sql<CustomersTableType[]>`
